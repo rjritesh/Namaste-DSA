6c44c7a1949f9552395ca44a8fa142d687c7d2f6
Ritesh <riteshjha24060@gmail.com>
2025-06-29 16:50:13 +0530

    Added problems on function & if-else

--- warm-up/function,if-else/script.js
+++ warm-up/function,if-else/script.js
@@ -18,3 +18,30 @@ console.log(square(12));
 
 
 //Write a program that accepts a number (age) and checks whether the person is eligible to vote. A person is eligible if their age is 18 or more.
+function checkEligibility(age){
+if(age > 18 && age < 120){
+  console.log("You can vote");
+}
+else if(age < 0){
+  console.log("Enter a valid age");
+}
+else{
+  console.log("You can not vote");
+}
+}
+checkEligibility(19)
+
+
+//Write a function that accepts a number and checks whether it is Even or Odd. If the number is divisble by 2, it’s an Even number. Otherwise, it’s an Odd number. Test the function with inputs 18 and 5.
+
+
+function isEvenOrOdd(num){
+  if(num % 2 == 0){
+    console.log("Number is even");
+  }
+  else{
+    console.log("Number is odd");
+  }
+}
+isEvenOrOdd(5)
+
