939494f95d6edd587a7d58581866361e6d40060e
Ritesh <riteshjha24060@gmail.com>
2025-07-02 12:22:10 +0530

    Tackled corner cases

--- warm-up/CountDigit/script.js
+++ warm-up/CountDigit/script.js
@@ -1,4 +1,8 @@
 function CountDigit(n) {
+  if(n === 0){
+    return 1;
+  }
+  n = Math.abs(n);
   let count = 0;
   while (n > 0) {
     n = Math.floor(n / 10);
@@ -7,4 +11,4 @@ function CountDigit(n) {
   return count;
 }
 
-console.log(CountDigit(6338));
+console.log(CountDigit(-1222));
