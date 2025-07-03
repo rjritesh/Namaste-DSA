be9b7fc66eca24407f0bc927b7384372749004ed
Ritesh <riteshjha24060@gmail.com>
2025-07-03 16:15:44 +0530

    Solved reverse integer problem

---
+++ warm-up/reverse-Integer/script.js
@@ -0,0 +1,19 @@
+function reverseInteger(x) {
+  let xcopy = x;
+  let rev = 0;
+
+  x = Math.abs(x);
+  while (x > 0) {
+    let rem = x % 10;
+    rev = rev * 10 + rem;
+    x = Math.floor(x / 10);
+  }
+  if (xcopy <  0 ) {
+    return -rev;
+  } else {
+    return rev;
+  }
+}
+
+let n = 523;
+console.log(reverseInteger(n));
