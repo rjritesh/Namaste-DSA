f301677a8484b953062641f3ef01df10a8dd3d49
Ritesh <97453505+rjritesh@users.noreply.github.com>
2025-08-05 11:37:09 +0530

    Optimized approach

--- 02 Arrays- level 0/Max Consecutive Ones/script.js
+++ 02 Arrays- level 0/Max Consecutive Ones/script.js
@@ -16,3 +16,24 @@ function maxConsecutiveOnes(nums){
 
 let array = [1,1,1,1,1,0,1,1,1];
 console.log(maxConsecutiveOnes(array));
+
+
+//Another approach
+
+
+var findMaxConsecutiveOnes = function(nums) {
+    let count = 0;
+    let maxCount = 0;
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i]=== 1){
+            count++;
+            maxCount = Math.max(count, maxCount);
+        }
+        else{
+            count = 0;
+        }
+    }
+    return maxCount
+};
+
+
