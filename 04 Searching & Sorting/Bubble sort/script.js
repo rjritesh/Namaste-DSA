bf2cc8462677b0d495b87d88b0ea2103857b8366
Ritesh <riteshjha24060@gmail.com>
2025-08-11 12:30:48 +0530

    Bubble sort

---
+++ 04 Searching & Sorting/Bubble sort/script.js
@@ -0,0 +1,16 @@
+function sort(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+let array = [5, 4, 9, 15, 10, 0];
+console.log(sort(array))
