82bfd0934e570033766c03e86aee105fc9017dd1
Ritesh <riteshjha24060@gmail.com>
2025-08-11 13:04:07 +0530

    Optimized Bubble Sort

--- 04 Searching & Sorting/Bubble sort/script.js
+++ 04 Searching & Sorting/Bubble sort/script.js
@@ -1,13 +1,16 @@
 function sort(arr) {
   let n = arr.length;
+  let swapped = false;
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        swapped = true;
       }
     }
+    if(!swapped) break;
   }
   return arr;
 }
