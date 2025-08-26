10b13cbe045ca369bb1248209541fe36418556df
Ritesh <riteshjha24060@gmail.com>
2025-08-26 12:31:05 +0530

    Non recursive approach

--- 02 Arrays- level 0/Missing number/script.js
+++ 02 Arrays- level 0/Missing number/script.js
@@ -1,3 +1,17 @@
+// function missingNumber(nums){
+//   let n = nums.length;
+//   let totalSum = n * ( n + 1) / 2;
+//   let sum = 0;
+//   for(let i = 0; i < n; i++){
+//     sum = sum + nums[i]
+//   }
+
+//   return totalSum - sum;
+// }
+
+// let array = [3,0,1,2,5,4];
+// console.log(missingNumber(array));
+
 function missingNumber (nums){
   nums.sort((a,b)=> a- b);
   for(let i = 0; i < nums.length; i++){
--- 03 Recursion/Power of two/script.js
+++ 03 Recursion/Power of two/script.js
@@ -7,4 +7,16 @@ function isPowerOfTwo(n) {
   return isPowerOfTwo(n / 2);
 }
 
-console.log(isPowerOfTwo(160))
+console.log(isPowerOfTwo(4));
+
+//Non recursive approach
+
+function isPow(n) {
+  if (n <= 1) return false;
+  while (n % 2 === 0) {
+    n = n / 2;
+  }
+  return n === 1
+}
+
+console.log(isPow(4))
