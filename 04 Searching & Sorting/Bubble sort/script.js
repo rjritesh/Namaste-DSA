1d04a889e38af3903e7d43b2d46160005e5e4a48
Ritesh <riteshjha24060@gmail.com>
2025-08-29 11:33:17 +0530

    Optimize bubble sort

--- 04 Searching & Sorting/Bubble sort/script.js
+++ 04 Searching & Sorting/Bubble sort/script.js
@@ -1,16 +1,19 @@
 function bubbleSort(nums){
   let n = nums.length;
   for(let i = 0; i < n - 1; i++){
+    let swapped = false;
     for(j = 0; j < n - 1 - i; j++){
       if(nums[j] > nums[j + 1]){
         let temp = nums[j];
         nums[j] = nums[j + 1];
         nums[j + 1] = temp;
+        swapped = true;
       }
     }
+    if(!swapped) break;
   }
   return nums;
 }
 
-let arr = [ 5, 2, 4, 1];
+let arr = [1,2,3,4,5];
 console.log(bubbleSort(arr));
