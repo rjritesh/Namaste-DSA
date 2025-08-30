7b480e900f68cf7df20455a8433473d042c414d0
Ritesh <riteshjha24060@gmail.com>
2025-08-30 11:58:02 +0530

    Merge sorted array using extra space

--- 02 Arrays- level 0/Merge sorted array/script.js
+++ 02 Arrays- level 0/Merge sorted array/script.js
@@ -12,9 +12,41 @@ function merge(nums1, nums2, m, n) {
     }
     k--;
   }
-  while(j >= 0){
+  while (j >= 0) {
     nums1[k] = nums2[j];
     j--;
     k--;
   }
 }
+
+//Using extra space
+
+function sort(arr1, arr2) {
+  let i = 0;
+  j = 0;
+  let sorted = [];
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      sorted.push(arr1[i]);
+      i++;
+    }
+    else{
+      sorted.push(arr2[j]);
+      j++;
+    }
+  }
+  while(i < arr1.length){
+    sorted.push(arr1[i]);
+    i++;
+  }
+  while(j < arr2.length){
+    sorted.push(arr2[j]);
+    j++;
+  }
+  return sorted;
+}
+
+let a1 = [ 1,3,5,7];
+let a2 = [2,4,8,9];
+console.log((sort(a1, a2)));
